ed1a347cc9e0bd36b1644f364b07e7e6b1ce8cf4
YaoChen5566 <dick02040204@gmail.com>
2016-12-05 20:42:03 +0800

    20161205 final edit
    change vector into Matrix

--- main.cpp
+++ main.cpp
@@ -18,11 +18,11 @@ using namespace std;
 using namespace cv;
 using namespace boost;
 
-void undirectedGraph();
+//void undirectedGraph();
 
 int main()
 {
-
+	clock_t start = clock(); // compare start
 	Mat input = imread("man.jpg");
 
 	vector<Mat> channels;
@@ -33,35 +33,39 @@ int main()
 	Mat R = channels[2];
 
 	cout <<"width: "<<input.cols<<", height: "<<input.rows<<endl;;
-	vector< vector<int> > edgeMatrix(input.cols*input.rows, vector<int>(input.cols*input.rows)); //edgeMatrix[2][1]
-
+	//vector< vector<int> > edgeMatrix(input.cols*input.rows, vector<int>(input.cols*input.rows)); //edgeMatrix[2][1]
 	
-	for(int i = 0 ; i < input.cols*input.rows ; i++)
-		for(int j = 0 ; j < input.cols*input.rows ; j++)
-			edgeMatrix[i][j] = 0;
-		
+	Mat edgeMatrixMat = Mat::zeros(input.cols*input.rows, input.cols*input.rows, CV_32FC1);
+	
+	//for(int i = 0 ; i < input.cols*input.rows ; i++)
+	//	for(int j = 0 ; j < input.cols*input.rows ; j++)
+	//		edgeMatrix[i][j] = 0;
+	//	
 
 	for(int i = 1 ; i < input.cols-1 ; i++)
 	{
 		for(int j = 1 ; j < input.rows-1 ; j++)
 		{
 			//x-1
-			edgeMatrix[j*input.cols+i][j*input.cols+i-1] = sqrt(pow(B.at<float>(j, i)-B.at<float>(j, i-1), 2) + pow(G.at<float>(j, i)-G.at<float>(j, i-1), 2) + pow(R.at<float>(j, i)-R.at<float>(j, i-1), 2));
-			edgeMatrix[j*input.cols+i-1][j*input.cols+i] = sqrt(pow(B.at<float>(j, i)-B.at<float>(j, i-1), 2) + pow(G.at<float>(j, i)-G.at<float>(j, i-1), 2) + pow(R.at<float>(j, i)-R.at<float>(j, i-1), 2));
+			edgeMatrixMat.at<double>(j*input.cols+i, j*input.cols+i-1) = sqrt(pow(B.at<float>(j, i)-B.at<float>(j, i-1), 2) + pow(G.at<float>(j, i)-G.at<float>(j, i-1), 2) + pow(R.at<float>(j, i)-R.at<float>(j, i-1), 2));
+			edgeMatrixMat.at<double>(j*input.cols+i-1, j*input.cols+i) = sqrt(pow(B.at<float>(j, i)-B.at<float>(j, i-1), 2) + pow(G.at<float>(j, i)-G.at<float>(j, i-1), 2) + pow(R.at<float>(j, i)-R.at<float>(j, i-1), 2));
 			//x-w
-			edgeMatrix[j*input.cols+i][j*input.cols+i-input.cols] = sqrt(pow(B.at<float>(j-1, i)-B.at<float>(j-1, i), 2)+pow(G.at<float>(j-1, i)-G.at<float>(j-1, i), 2)+pow(R.at<float>(j-1, i)-R.at<float>(j-1, i), 2));
-			edgeMatrix[j*input.cols+i-input.cols][j*input.cols+i] = sqrt(pow(B.at<float>(j-1, i)-B.at<float>(j-1, i), 2)+pow(G.at<float>(j-1, i)-G.at<float>(j-1, i), 2)+pow(R.at<float>(j-1, i)-R.at<float>(j-1, i), 2));
+			edgeMatrixMat.at<double>(j*input.cols+i, j*input.cols+i-input.cols) = sqrt(pow(B.at<float>(j-1, i)-B.at<float>(j-1, i), 2)+pow(G.at<float>(j-1, i)-G.at<float>(j-1, i), 2)+pow(R.at<float>(j-1, i)-R.at<float>(j-1, i), 2));
+			edgeMatrixMat.at<double>(j*input.cols+i-input.cols, j*input.cols+i) = sqrt(pow(B.at<float>(j-1, i)-B.at<float>(j-1, i), 2)+pow(G.at<float>(j-1, i)-G.at<float>(j-1, i), 2)+pow(R.at<float>(j-1, i)-R.at<float>(j-1, i), 2));
 			//x+1
-			edgeMatrix[j*input.cols+i][j*input.cols+i+1] = sqrt(pow(B.at<float>(j, i)-B.at<float>(j, i+1), 2) + pow(G.at<float>(j, i)-G.at<float>(j, i+1), 2) + pow(R.at<float>(j, i)-R.at<float>(j, i+1), 2));
-			edgeMatrix[j*input.cols+i+1][j*input.cols+i] = sqrt(pow(B.at<float>(j, i)-B.at<float>(j, i+1), 2) + pow(G.at<float>(j, i)-G.at<float>(j, i+1), 2) + pow(R.at<float>(j, i)-R.at<float>(j, i+1), 2));
+			edgeMatrixMat.at<double>(j*input.cols+i, j*input.cols+i+1) = sqrt(pow(B.at<float>(j, i)-B.at<float>(j, i+1), 2) + pow(G.at<float>(j, i)-G.at<float>(j, i+1), 2) + pow(R.at<float>(j, i)-R.at<float>(j, i+1), 2));
+			edgeMatrixMat.at<double>(j*input.cols+i+1, j*input.cols+i) = sqrt(pow(B.at<float>(j, i)-B.at<float>(j, i+1), 2) + pow(G.at<float>(j, i)-G.at<float>(j, i+1), 2) + pow(R.at<float>(j, i)-R.at<float>(j, i+1), 2));
 			//x+w
-			edgeMatrix[j*input.cols+i][j*input.cols+i+input.cols] = sqrt(pow(B.at<float>(j+1, i)-B.at<float>(j+1, i), 2)+pow(G.at<float>(j+1, i)-G.at<float>(j+1, i), 2)+pow(R.at<float>(j+1, i)-R.at<float>(j+1, i), 2));
-			edgeMatrix[j*input.cols+i+input.cols][j*input.cols+i] = sqrt(pow(B.at<float>(j+1, i)-B.at<float>(j+1, i), 2)+pow(G.at<float>(j+1, i)-G.at<float>(j+1, i), 2)+pow(R.at<float>(j+1, i)-R.at<float>(j+1, i), 2));
+			edgeMatrixMat.at<double>(j*input.cols+i, j*input.cols+i+input.cols) = sqrt(pow(B.at<float>(j+1, i)-B.at<float>(j+1, i), 2)+pow(G.at<float>(j+1, i)-G.at<float>(j+1, i), 2)+pow(R.at<float>(j+1, i)-R.at<float>(j+1, i), 2));
+			edgeMatrixMat.at<double>(j*input.cols+i+input.cols, j*input.cols+i) = sqrt(pow(B.at<float>(j+1, i)-B.at<float>(j+1, i), 2)+pow(G.at<float>(j+1, i)-G.at<float>(j+1, i), 2)+pow(R.at<float>(j+1, i)-R.at<float>(j+1, i), 2));
 		}
 	}
 
 	cout <<"edge done"<<endl;
+	
+	clock_t finish = clock(); // compare finish
 
+	cout << "time: " << finish-start<<endl;
 	//typedef boost::undirected_graph<> Graph;
 	//Graph inputGraph;
 
@@ -89,14 +93,14 @@ int main()
 	return 0;
 }
 
-void undirectedGraph()
-{
-	typedef boost::undirected_graph<> Graph;
-
-	Graph g;
-
-	for(int i = 0 ; i < 10 ; i++)
-	{
-		g.add_vertex();
-	}
-}
+//void undirectedGraph()
+//{
+//	typedef boost::undirected_graph<> Graph;
+//
+//	Graph g;
+//
+//	for(int i = 0 ; i < 10 ; i++)
+//	{
+//		g.add_vertex();
+//	}
+//}
