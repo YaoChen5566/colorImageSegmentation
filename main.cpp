485df0bb73ea950e7c76fe31dc3e60c328590751
YaoChen5566 <dick02040204@gmail.com>
2016-12-05 17:55:55 +0800

    20161205
    set up the edge information

---
+++ main.cpp
@@ -0,0 +1,102 @@
+#include <opencv\cv.h>
+#include <opencv2\highgui\highgui.hpp>
+#include <iostream>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <dirent.h>
+#include <errno.h>
+
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/undirected_graph.hpp>
+
+using namespace std;
+using namespace cv;
+using namespace boost;
+
+void undirectedGraph();
+
+int main()
+{
+
+	Mat input = imread("man.jpg");
+
+	vector<Mat> channels;
+	split(input, channels);
+
+	Mat B = channels[0];
+	Mat G = channels[1];
+	Mat R = channels[2];
+
+	cout <<"width: "<<input.cols<<", height: "<<input.rows<<endl;;
+	vector< vector<int> > edgeMatrix(input.cols*input.rows, vector<int>(input.cols*input.rows)); //edgeMatrix[2][1]
+
+	
+	for(int i = 0 ; i < input.cols*input.rows ; i++)
+		for(int j = 0 ; j < input.cols*input.rows ; j++)
+			edgeMatrix[i][j] = 0;
+		
+
+	for(int i = 1 ; i < input.cols-1 ; i++)
+	{
+		for(int j = 1 ; j < input.rows-1 ; j++)
+		{
+			//x-1
+			edgeMatrix[j*input.cols+i][j*input.cols+i-1] = sqrt(pow(B.at<float>(j, i)-B.at<float>(j, i-1), 2) + pow(G.at<float>(j, i)-G.at<float>(j, i-1), 2) + pow(R.at<float>(j, i)-R.at<float>(j, i-1), 2));
+			edgeMatrix[j*input.cols+i-1][j*input.cols+i] = sqrt(pow(B.at<float>(j, i)-B.at<float>(j, i-1), 2) + pow(G.at<float>(j, i)-G.at<float>(j, i-1), 2) + pow(R.at<float>(j, i)-R.at<float>(j, i-1), 2));
+			//x-w
+			edgeMatrix[j*input.cols+i][j*input.cols+i-input.cols] = sqrt(pow(B.at<float>(j-1, i)-B.at<float>(j-1, i), 2)+pow(G.at<float>(j-1, i)-G.at<float>(j-1, i), 2)+pow(R.at<float>(j-1, i)-R.at<float>(j-1, i), 2));
+			edgeMatrix[j*input.cols+i-input.cols][j*input.cols+i] = sqrt(pow(B.at<float>(j-1, i)-B.at<float>(j-1, i), 2)+pow(G.at<float>(j-1, i)-G.at<float>(j-1, i), 2)+pow(R.at<float>(j-1, i)-R.at<float>(j-1, i), 2));
+			//x+1
+			edgeMatrix[j*input.cols+i][j*input.cols+i+1] = sqrt(pow(B.at<float>(j, i)-B.at<float>(j, i+1), 2) + pow(G.at<float>(j, i)-G.at<float>(j, i+1), 2) + pow(R.at<float>(j, i)-R.at<float>(j, i+1), 2));
+			edgeMatrix[j*input.cols+i+1][j*input.cols+i] = sqrt(pow(B.at<float>(j, i)-B.at<float>(j, i+1), 2) + pow(G.at<float>(j, i)-G.at<float>(j, i+1), 2) + pow(R.at<float>(j, i)-R.at<float>(j, i+1), 2));
+			//x+w
+			edgeMatrix[j*input.cols+i][j*input.cols+i+input.cols] = sqrt(pow(B.at<float>(j+1, i)-B.at<float>(j+1, i), 2)+pow(G.at<float>(j+1, i)-G.at<float>(j+1, i), 2)+pow(R.at<float>(j+1, i)-R.at<float>(j+1, i), 2));
+			edgeMatrix[j*input.cols+i+input.cols][j*input.cols+i] = sqrt(pow(B.at<float>(j+1, i)-B.at<float>(j+1, i), 2)+pow(G.at<float>(j+1, i)-G.at<float>(j+1, i), 2)+pow(R.at<float>(j+1, i)-R.at<float>(j+1, i), 2));
+		}
+	}
+
+	cout <<"edge done"<<endl;
+
+	//typedef boost::undirected_graph<> Graph;
+	//Graph inputGraph;
+
+	//for(int i = 0 ; i < input.cols ; i++)
+	//{
+	//	for(int j = 0 ; j < input.rows ; j++)
+	//	{
+	//		inputGraph.add_vertex();
+	//	}
+	//}
+
+	//for(int i = 0 ; i < input.cols ; i++)
+	//{
+	//	for(int j = 0 ; j < input.rows ; j++)
+	//	{
+	//		inputGraph.add_edge(1,2,10);
+	//	}
+	//}
+
+
+	
+	//undirectedGraph();
+	system("Pause");
+
+	return 0;
+}
+
+void undirectedGraph()
+{
+	typedef boost::undirected_graph<> Graph;
+
+	Graph g;
+
+	for(int i = 0 ; i < 10 ; i++)
+	{
+		g.add_vertex();
+	}
+}
