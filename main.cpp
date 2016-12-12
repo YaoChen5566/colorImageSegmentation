c37e9ad59661a8113647672ffa869e1e167ef88a
YaoChen5566 <dick02040204@gmail.com>
2016-12-12 16:02:17 +0800

    20161212 get histogram with Hue
    convert BGR2HSV and use Hue channel to draw a histogram

--- main.cpp
+++ main.cpp
@@ -10,97 +10,40 @@
 #include <dirent.h>
 #include <errno.h>
 
-#include <boost/graph/graph_traits.hpp>
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/undirected_graph.hpp>
-
 using namespace std;
 using namespace cv;
-using namespace boost;
-
-//void undirectedGraph();
 
 int main()
 {
 	clock_t start = clock(); // compare start
-	Mat input = imread("man.jpg");
+	Mat input = imread("duck.jpg");
+
+	Mat inputHSV;
+
+	cvtColor(input, inputHSV, CV_BGR2HSV);
 
 	vector<Mat> channels;
-	split(input, channels);
+	split(inputHSV, channels);
 
-	Mat B = channels[0];
-	Mat G = channels[1];
-	Mat R = channels[2];
+	Mat H = channels[0];
+	Mat S = channels[1];
+	Mat V = channels[2];
 
-	cout <<"width: "<<input.cols<<", height: "<<input.rows<<endl;;
-	//vector< vector<int> > edgeMatrix(input.cols*input.rows, vector<int>(input.cols*input.rows)); //edgeMatrix[2][1]
-	
-	Mat edgeMatrixMat = Mat::zeros(input.cols*input.rows, input.cols*input.rows, CV_32FC1);
-	
-	//for(int i = 0 ; i < input.cols*input.rows ; i++)
-	//	for(int j = 0 ; j < input.cols*input.rows ; j++)
-	//		edgeMatrix[i][j] = 0;
-	//	
+	int histSize = 181;
 
-	for(int i = 1 ; i < input.cols-1 ; i++)
-	{
-		for(int j = 1 ; j < input.rows-1 ; j++)
-		{
-			//x-1
-			edgeMatrixMat.at<double>(j*input.cols+i, j*input.cols+i-1) = sqrt(pow(B.at<float>(j, i)-B.at<float>(j, i-1), 2) + pow(G.at<float>(j, i)-G.at<float>(j, i-1), 2) + pow(R.at<float>(j, i)-R.at<float>(j, i-1), 2));
-			edgeMatrixMat.at<double>(j*input.cols+i-1, j*input.cols+i) = sqrt(pow(B.at<float>(j, i)-B.at<float>(j, i-1), 2) + pow(G.at<float>(j, i)-G.at<float>(j, i-1), 2) + pow(R.at<float>(j, i)-R.at<float>(j, i-1), 2));
-			//x-w
-			edgeMatrixMat.at<double>(j*input.cols+i, j*input.cols+i-input.cols) = sqrt(pow(B.at<float>(j-1, i)-B.at<float>(j-1, i), 2)+pow(G.at<float>(j-1, i)-G.at<float>(j-1, i), 2)+pow(R.at<float>(j-1, i)-R.at<float>(j-1, i), 2));
-			edgeMatrixMat.at<double>(j*input.cols+i-input.cols, j*input.cols+i) = sqrt(pow(B.at<float>(j-1, i)-B.at<float>(j-1, i), 2)+pow(G.at<float>(j-1, i)-G.at<float>(j-1, i), 2)+pow(R.at<float>(j-1, i)-R.at<float>(j-1, i), 2));
-			//x+1
-			edgeMatrixMat.at<double>(j*input.cols+i, j*input.cols+i+1) = sqrt(pow(B.at<float>(j, i)-B.at<float>(j, i+1), 2) + pow(G.at<float>(j, i)-G.at<float>(j, i+1), 2) + pow(R.at<float>(j, i)-R.at<float>(j, i+1), 2));
-			edgeMatrixMat.at<double>(j*input.cols+i+1, j*input.cols+i) = sqrt(pow(B.at<float>(j, i)-B.at<float>(j, i+1), 2) + pow(G.at<float>(j, i)-G.at<float>(j, i+1), 2) + pow(R.at<float>(j, i)-R.at<float>(j, i+1), 2));
-			//x+w
-			edgeMatrixMat.at<double>(j*input.cols+i, j*input.cols+i+input.cols) = sqrt(pow(B.at<float>(j+1, i)-B.at<float>(j+1, i), 2)+pow(G.at<float>(j+1, i)-G.at<float>(j+1, i), 2)+pow(R.at<float>(j+1, i)-R.at<float>(j+1, i), 2));
-			edgeMatrixMat.at<double>(j*input.cols+i+input.cols, j*input.cols+i) = sqrt(pow(B.at<float>(j+1, i)-B.at<float>(j+1, i), 2)+pow(G.at<float>(j+1, i)-G.at<float>(j+1, i), 2)+pow(R.at<float>(j+1, i)-R.at<float>(j+1, i), 2));
-		}
-	}
+	float range[] = {0, 181};
+	const float* histRange = {range};
 
-	cout <<"edge done"<<endl;
-	
-	clock_t finish = clock(); // compare finish
+	bool uniform = true;
+	bool accumulate = false;
 
-	cout << "time: " << finish-start<<endl;
-	//typedef boost::undirected_graph<> Graph;
-	//Graph inputGraph;
+	Mat histH;
 
-	//for(int i = 0 ; i < input.cols ; i++)
-	//{
-	//	for(int j = 0 ; j < input.rows ; j++)
-	//	{
-	//		inputGraph.add_vertex();
-	//	}
-	//}
-
-	//for(int i = 0 ; i < input.cols ; i++)
-	//{
-	//	for(int j = 0 ; j < input.rows ; j++)
-	//	{
-	//		inputGraph.add_edge(1,2,10);
-	//	}
-	//}
+	calcHist(&H, 1, 0, Mat(), histH, 1, &histSize, &histRange, uniform, accumulate);
 
 
-	
-	//undirectedGraph();
 	system("Pause");
 
 	return 0;
 }
 
-//void undirectedGraph()
-//{
-//	typedef boost::undirected_graph<> Graph;
-//
-//	Graph g;
-//
-//	for(int i = 0 ; i < 10 ; i++)
-//	{
-//		g.add_vertex();
-//	}
-//}
